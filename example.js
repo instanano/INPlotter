49d51d0220fdcb71b0dc7e2fd5bd9d431af8be70
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-13 15:33:14 +0530

    Update example.js

--- example.js
+++ example.js
@@ -18,22 +18,4 @@ d3.select('.dchart').append('foreignObject')
  .style('display','inline-block')
  .on("contextmenu", function(data,index){d3.event.preventDefault()})
 
-
-
-d3.select(window).on("input", function(e) { 
-document.getElementById("gtitle").setAttribute("width", document.getElementById("dgtitle").scrollWidth + 20);
-document.getElementById("gtitle").setAttribute("height", document.getElementById("dgtitle").scrollHeight);
-
-document.getElementById("txaxis").setAttribute("width", document.getElementById("dtxaxis").scrollWidth + 20);
-document.getElementById("txaxis").setAttribute("height", document.getElementById("dtxaxis").scrollHeight);
-
-for (var i=1; i <= 50; i++){
-document.getElementById("peak"+i).setAttribute("width", document.getElementById("dpeak"+i).scrollWidth + 20);
-document.getElementById("peak"+i).setAttribute("height", document.getElementById("dpeak"+i).scrollHeight);
-}
-})
-
-d3.select("#xrd").on('click', function(){
-d3.select('#dtxaxis').html('2 Theta')
-})
 </script>
