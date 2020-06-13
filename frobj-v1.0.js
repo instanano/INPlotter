0c8d6a3ed057e3918e37a0ebfa8b342ff37c34ba
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-13 22:30:52 +0530

    Update frobj-v1.0.js

--- frobj-v1.0.js
+++ frobj-v1.0.js
@@ -1,10 +1,10 @@
 d3.select(window).on("input", function(e) { 
 document.getElementById("gtitle").setAttribute("width", document.getElementById("dgtitle").scrollWidth + 20);
-document.getElementById("gtitle").setAttribute("height", document.getElementById("dgtitle").scrollHeight);
+document.getElementById("gtitle").setAttribute("height", document.getElementById("dgtitle").scrollHeight + 5);
 document.getElementById("txaxis").setAttribute("width", document.getElementById("dtxaxis").scrollWidth + 20);
-document.getElementById("txaxis").setAttribute("height", document.getElementById("dtxaxis").scrollHeight);
+document.getElementById("txaxis").setAttribute("height", document.getElementById("dtxaxis").scrollHeight + 5);
 for (var i=1; i <= 50; i++){
 document.getElementById("peak"+i).setAttribute("width", document.getElementById("dpeak"+i).scrollWidth + 20);
-document.getElementById("peak"+i).setAttribute("height", document.getElementById("dpeak"+i).scrollHeight);
+document.getElementById("peak"+i).setAttribute("height", document.getElementById("dpeak"+i).scrollHeight + 5);
 }
 })
