6854ebd5e8ae6fbff18896bd710892216e7bdf9f
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-13 13:38:01 +0530

    Update addtext-v1.0.js

--- addtext-v1.0.js
+++ addtext-v1.0.js
@@ -1,14 +1,15 @@
+var o=1;var p=1;
 d3.select("#addtext").on('click', function(){
 d3.select('.dchart').append('foreignObject')
  .call(d3.drag().on("start", dragstarted).on("drag", dragged))
  .on("click",function(){if (d3.event.shiftKey) {this.remove()}})
- .attr('id','nfrobj')
+ .attr('id','nfrobj'+o++)
  .attr('x', 600)
  .attr('y', 200)
  .attr("width", "300px")
  .attr("height", "36px")
  .append('xhtml:div')
- .attr('id','ntext')
+ .attr('id','ntext'+p++)
  .attr("contentEditable", true)
  .style("cursor", "move")
  .html("Text")
