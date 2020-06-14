75b5f78319826ca83f2a26986ff11fb35f13c54b
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-14 22:41:30 +0530

    Delete test12.js

--- test12.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('dinput').addEventListener('paste', function (e) {
-document.getElementById('dinput').addEventListener('input', function () {
-d3.selectAll('svg').remove();
-drawChart(getData());
-});});
