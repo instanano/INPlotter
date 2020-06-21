24e4819b2a055822efc85c00562a9ecf22c86ec5
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-21 11:55:08 +0530

    Create addtext-v1.3.js

---
+++ addtext-v1.3.js
@@ -0,0 +1,23 @@
+d3.select("#addtext").on('click', function(){
+d3.select('.dchart').append("g").on("click",function(){
+    			$('foreignObject').on('click', function() {
+          $('foreignObject').removeClass("dhe");
+ 				  $(this).addClass('dhe')
+   				})})
+ .append('foreignObject')
+ .call(d3.drag().on("start", dragstarted).on("drag", dragged))
+ .attr('class','frobj')
+ .attr('x', 600)
+ .attr('y', 200)
+ .attr("width", "300px")
+ .attr("height", "36px")
+ .append('xhtml:div')
+ .attr('class','frdiv')
+ .attr("contentEditable", true)
+ .style("cursor", "move")
+ .html("Text")
+ .style('font-size','30px')
+ .style('display','inline-block')
+ .style('transform','translateY(7px)')
+ .on("contextmenu", function(data,index){d3.event.preventDefault()})
+})
