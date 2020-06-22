10e023b00725a8ca698c1d83614d68ec1610f769
InstaNANO <52311496+instanano@users.noreply.github.com>
2020-06-22 10:21:46 +0530

    Update frobj-v1.2.js

--- frobj-v1.2.js
+++ frobj-v1.2.js
@@ -1,6 +1,6 @@
   d3.select(window).on("input load change", function(e) {
     document.querySelectorAll(".frobj").forEach(el => {
-      el.setAttribute("width", el.querySelector(".frdiv").scrollWidth +20);
+      el.setAttribute("width", el.querySelector(".frdiv").scrollWidth + 30);
       el.setAttribute("height", el.querySelector(".frdiv").scrollHeight + 20);
     });    
   })
